c96d6a605499ddcf70ace328b88f9bf9f427e12e
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2020-06-27 12:32:28 +0530

    post request data

--- main.go
+++ main.go
@@ -31,22 +31,45 @@ func triplesString(triples map[string]*parser.Triple) string {
 	return op
 }
 
+type test_struct struct {
+	name string
+}
+
 func execute(w http.ResponseWriter, r *http.Request) {
 	filename := "temp.rdf"
-	r.ParseForm()
-	fmt.Println(ioutil.WriteFile(filename, []byte(r.FormValue("data")), 777))
+	if ioutil.WriteFile(filename, []byte(r.FormValue("data")), 777) != nil {
+		fmt.Fprint(w, "Internal server error: writing to file err")
+		return
+	}
+
+	err := r.ParseMultipartForm(2 * 1024 * 1024)
+	if err != nil {
+		fmt.Fprint(w, "failed to get input data. network issue.")
+		return
+	}
+	fmt.Println(r.FormValue("data"))
 	rdfParser := parser.New()
-	err := rdfParser.Parse(filename)
-	os.Remove(filename)
+	err = rdfParser.Parse(filename)
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
+		return
 	} else {
+		err = os.Remove(filename)
+		if err != nil {
+			fmt.Fprint(w, "error removing temporary file")
+		}
 		fmt.Println(fmt.Fprintf(w, triplesString(rdfParser.Triples)))
+		return
 	}
 }
 
+
 func main() {
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
+	fmt.Println(port)
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/get", execute)
 	log.Fatal(http.ListenAndServe(":" + port, nil))
